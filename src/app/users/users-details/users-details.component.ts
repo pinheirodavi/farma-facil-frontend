c38a21ff30c76b065bc18d82fc990103c69e2501
Davi Pinheiro <davi.pinheiro@itbeta2.com.br>
2022-12-04 00:12:04 -0300

    connect backend to users table

--- src/app/users/users-details/users-details.component.ts
+++ src/app/users/users-details/users-details.component.ts
@@ -1,15 +1,51 @@
+import { UsersService } from './../services/users.service';
+import { AuthenticationService } from 'src/app/authentication/services/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-users-details',
   templateUrl: './users-details.component.html',
-  styleUrls: ['./users-details.component.scss']
+  styleUrls: ['./users-details.component.scss'],
 })
 export class UsersDetailsComponent implements OnInit {
+  constructor(
+    private service: UsersService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private form: FormBuilder,
+    public dialog: MatDialog
+  ) {}
 
-  constructor() { }
+  roleList: any[] = [{ role: 'Administrador' }, { role: 'Funcionário' }];
+  isEdit = false;
+  id!: string;
+
+  usersForm = this.form.group({
+    nome: ['', Validators.required],
+    email: ['', Validators.required],
+    senha: ['', Validators.required],
+    confirmSenha: ['', Validators.required],
+  })
 
   ngOnInit(): void {
+    this.route.params.subscribe({
+      next: (params) => {
+        this.isEdit = params['id'] !== 'new';
+        this.id = params['id'];
+      }
+    })
+
+    if (this.isEdit) {
+      // this.service.findUserById(this.id).subscribe( ({name, email, role}) => {
+      // this.form.setValue({name, email, role},{emitEvent: false})
+      // })
+    }
   }
 
+  patch() {
+
+  }
 }
--- src/app/users/users-table/users-table.component.ts
+++ src/app/users/users-table/users-table.component.ts
@@ -1,6 +1,6 @@
+import { UsersService } from './../services/users.service';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-
 @Component({
   selector: 'app-users-table',
   templateUrl: './users-table.component.html',
@@ -10,21 +10,28 @@ export class UsersTableComponent implements OnInit {
 
   userDataSource = new MatTableDataSource<any>([]);
 
-  displayedColumns = ['username','name','email','profile','action']
+  displayedColumns = ['name','email','role','action']
 
+  // [
+  //   {username: "user1",name: "Usuário 1", email:"user1@email.com",profile:"Administrador"},
+  //   {username: "user2",name: "Usuário 2", email:"user2@email.com",profile:"Funcionário"},
+  //   {username: "user3",name: "Usuário 3", email:"user3@email.com",profile:"Funcionário"},
+  //   {username: "user4",name: "Usuário 4", email:"user4@email.com",profile:"Funcionário"},
+  //   {username: "user5",name: "Usuário 5", email:"user5@email.com",profile:"Funcionário"},
+  //   {username: "user6",name: "Usuário 6", email:"user6@email.com",profile:"Funcionário"},
+  // ]
 
-
-  constructor() { }
+  constructor(
+    private service: UsersService
+  ) { }
 
   ngOnInit(): void {
-    this.userDataSource.data = [
-      {username: "user1",name: "Usuário 1", email:"user1@email.com",profile:"Administrador"},
-      {username: "user2",name: "Usuário 2", email:"user2@email.com",profile:"Funcionário"},
-      {username: "user3",name: "Usuário 3", email:"user3@email.com",profile:"Funcionário"},
-      {username: "user4",name: "Usuário 4", email:"user4@email.com",profile:"Funcionário"},
-      {username: "user5",name: "Usuário 5", email:"user5@email.com",profile:"Funcionário"},
-      {username: "user6",name: "Usuário 6", email:"user6@email.com",profile:"Funcionário"},
-    ]
+    this.service.findUsers().subscribe(response => {
+      console.log(response)
+      this.userDataSource.data = response;
+    }, error => {
+      console.log('Não foi possível listar os usuários')
+    })
   }
 
   deleteUser(element: any){}
