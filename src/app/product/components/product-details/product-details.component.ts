2ea0d25be3c7ea04ac8256d60c44ea5787cb76ce
jose <jose.rodrigo@itbeta2.com.br>
2022-12-04 00:03:59 -0300

    create product form

--- src/app/product/components/product-details/product-details.component.ts
+++ src/app/product/components/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  productForm = this.form.group({
+    name: ['', Validators.required],
+    ppurchase: ['', Validators.required],
+    psale: ['', Validators.required],
+    provider: ['', Validators.required],
+    inventory: ['', Validators.required],
+    stripe: ['', Validators.required],
+    revenue: ['', Validators.required],
+    generic: ['', Validators.required]
+  });
+
+  constructor(
+    private form: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
